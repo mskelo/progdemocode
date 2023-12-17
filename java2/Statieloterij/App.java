9d35fab3f0b9187f0ba7d162a1af1873526985a0
Matej Skelo <matej@skelo.nl>
2023-12-17 21:59:36 +0100

    oops. forgot to multiply checkLot output by 0.2. 4 correct numbers now doesn't earn 4*jackpot anymore lol

--- java2/Statieloterij/App.java
+++ java2/Statieloterij/App.java
@@ -1,12 +1,13 @@
 public class App {
     public static void main(String[] args) {
         int jackpot = 10;
-        long wingeld = 0;  // int, long, double of float?
+        double wingeld = 0;  // int, long, double of float?
         for (int i = 0; i < LotteryNumbers.winningNumbers.length; i++) {
             for (int j = 0; j < LotteryNumbers.lotteryTickets.length; j++) {
                 // We zitten 2 tabs diep en hebben nog meer loops nodig
                 // Zet die loops weg in een losse functie om de code leesbaar te houden
-                wingeld +=   checkLot(LotteryNumbers.winningNumbers[i], 
+                wingeld +=   0.2
+                           * checkLot(LotteryNumbers.winningNumbers[i], 
                                       LotteryNumbers.lotteryTickets[j])
                            * jackpot; 
             }
@@ -14,10 +15,10 @@ public class App {
         }
         System.out.println("Totaal wingeld: "+wingeld/50+" euro.");
         // vergeet de 1/50 niet :^)
-        // int:    -40567080
-        // float:  2.17130928E8
-        // long:   217130957
-        // double: 2.1713095708E8
+        // int:    42949672
+        // float:  6.060606E7
+        // long:   60606060
+        // double: 6.06060606E7
     }
 
     // Returnt int tussen 0 en 5 (eigenlijk win.length) 
