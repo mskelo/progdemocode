48f03bf4cffa71153a5f33b0f03bf61ccae4f5eb
Matej Skelo <skelm@hr.nl>
2023-12-22 13:45:20 +0100

    part 1 done. input file needed 2 newlines at the end

--- java2/Kruiwagens/Gegeven.java
+++ java2/Kruiwagens/Gegeven.java
@@ -1,8 +1,65 @@
-// Implement an Orders class to manage the orders for vehicles
-class Order {
-    private  
+import java.util.Random;
+
+/**
+ * Hoeft niet verder ingevuld te worden
+ * `final class` betekent: "cannot be extended"
+ */
+final class Order {
+    private int[] orderedVehicles = new int[5]; 
+    public Order(int a, int b, int c, int d, int e) {
+        this.orderedVehicles[0] = a; // == kruiwagens
+        this.orderedVehicles[1] = b; // == fiets met motor
+        this.orderedVehicles[2] = c; // == fiets zonder motor
+        this.orderedVehicles[3] = d; // == bakfiets met motor
+        this.orderedVehicles[4] = e; // == bakfiets zonder motor
+    }
+    public int[] getOrderedVehicles() {
+        return this.orderedVehicles;
+    }
 }
 
-class InputValues {
+/**
+ * Vul zelf verder in en schrijf afgeleide classes in een apart .java bestand
+ */
+class Vehicle {
+    private int condition;
+    private int price;
 
+    // Constructor for Vehicle class
+    public Vehicle() {
+        Random random = new Random(-229985452); // Dit staat al voor je ingevuld
+        // Genereer ^ hiermee ^ de conditie van het voertuig en sla op in this.condition
+    }
+    
+    // Vul zelf verder in waar nodig na het maken van een klassendiagram
+    // ...
+}
+
+/**
+ * Read-only
+ */
+class InputValues {
+    // Zie VehicleFactory.main();
+    public final static Order[] allOrders = {
+        new Order(12, 45, 31, 8, 22),
+        new Order(6, 18, 40, 11, 49),
+        new Order(27, 5, 33, 14, 37),
+        new Order(9, 36, 0, 2, 47),
+        new Order(3, 29, 25, 42, 10),
+        new Order(16, 44, 19, 39, 4),
+        new Order(30, 21, 46, 1, 28),
+        new Order(34, 15, 23, 38, 20),
+        new Order(0, 26, 7, 41, 32),
+        new Order(24, 13, 48, 35, 50),
+        new Order(42, 18, 37, 9, 26),
+        new Order(5, 30, 14, 47, 21),
+        new Order(33, 10, 24, 0, 45),
+        new Order(19, 6, 50, 29, 12),
+        new Order(7, 41, 22, 3, 36),
+        new Order(49, 25, 8, 44, 16),
+        new Order(27, 1, 35, 0, 48),
+        new Order(23, 4, 32, 17, 40),
+        new Order(15, 28, 43, 20, 2),
+        new Order(34, 13, 31, 46, 39),
+    };
 }
--- java2/Kruiwagens/VehicleFactory.java
+++ java2/Kruiwagens/VehicleFactory.java
@@ -1,35 +1,13 @@
-// Define a Vehicle class as the superclass
-class Vehicle {
-    private String partNumber;
-    private int condition;
-    private int price;
+import java.util.Arrays;
 
-    // Constructor for Vehicle class
-    public Vehicle(String partNumber /* other common parameters */) {
-        this.partNumber = partNumber;
-        // Initialize common fields
-        // ...
-    }
-    
-    // Other common methods or fields for all vehicles
-    // ...
-}
-
-
-// Other subclasses representing different types of vehicles can be added similarly
-
-// Main class to test the functionality
 public class VehicleFactory {
-    public static /* int? */ processOrder(Order order) {
-        // while(invalidCount > 0) {
-        //     for () {
-
-        //     }
-        // }
-    }
-
     public static void main(String[] args) {
-        // Test your classes here
-        // ...
+        Order[] allOrders = InputValues.allOrders;
+        System.out.println("De eerste bestelling bevat: "+ Arrays.toString(allOrders[0].getOrderedVehicles()));
+        
+        // Verwerk Orders één voor één volgens de opdracht 
+        
+        // Tip: schrijf helper-methods om de puzzel in kleinere delen op te knippen,
+        //      en kies duidelijke namen om je code leesbaar te houden
     }
 }
