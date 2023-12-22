83c7605cd8d00f5bcfb9650f5384f50110bcbce8
Matej Skelo <skelm@hr.nl>
2023-12-22 12:45:37 +0100

    update opdracht

---
+++ java2/Kruiwagens/VehicleFactory.java
@@ -0,0 +1,35 @@
+// Define a Vehicle class as the superclass
+class Vehicle {
+    private String partNumber;
+    private int condition;
+    private int price;
+
+    // Constructor for Vehicle class
+    public Vehicle(String partNumber /* other common parameters */) {
+        this.partNumber = partNumber;
+        // Initialize common fields
+        // ...
+    }
+    
+    // Other common methods or fields for all vehicles
+    // ...
+}
+
+
+// Other subclasses representing different types of vehicles can be added similarly
+
+// Main class to test the functionality
+public class VehicleFactory {
+    public static /* int? */ processOrder(Order order) {
+        // while(invalidCount > 0) {
+        //     for () {
+
+        //     }
+        // }
+    }
+
+    public static void main(String[] args) {
+        // Test your classes here
+        // ...
+    }
+}
