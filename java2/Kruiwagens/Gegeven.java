a2dc05dc9387e5c0c5e230985824fc27c11ed29a
Matej Skelo <skelm@hr.nl>
2023-12-22 12:03:18 +0100

    update opdracht

---
+++ java2/Kruiwagens/Gegeven.java
@@ -0,0 +1,15 @@
+// Implement an Orders class to manage the orders for vehicles
+class Orders {
+    // Collection to store orders
+    // ...
+
+    // Method to process orders and create vehicles until all orders are filled
+    public void processOrders() {
+        // Logic to create vehicles, calculate checksums, and fill orders
+        // ...
+    }
+}
+
+class InputValues {
+    
+}
