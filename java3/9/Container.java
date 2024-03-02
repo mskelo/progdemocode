9e10a22b5e9fbcd237e9f06a7f9bfdcd5b6b1bcc
Matej Skelo <matej@skelo.nl>
2024-03-02 21:52:40 +0100

    SPO les 9

--- java3/9/Container.java
+++ java3/9/Container.java
@@ -1,10 +1,15 @@
 public class Container {
     private int id;
-    public Container(int id) {
+    private String inhoud;
+    public Container(int id, String inhoud) {
         this.id = id;
+        this.inhoud = inhoud;
     }
     public int getID() {
         return this.id;
+    }
+    public String getInhoud() {
+        return this.inhoud;
     }
 }
 
---
+++ java3/9/InputVrachtwagens.java
@@ -0,0 +1,36 @@
+public final class InputVrachtwagens { // final voorkomt overerving van deze class
+    private InputVrachtwagens() {}     // private constructor voorkomt instantiering van deze class
+                                       
+    public static final Vrachtwagen[] aangemelde_vrachtwagens = {
+        new Vrachtwagen("J-708-WW", new Container(0, "Sinaasappels")),
+        new Vrachtwagen("K-405-PL", new Container(1, "Arduino's")),
+        new Vrachtwagen("A-744-RT", new Container(2, "Blauw zout")),
+        new Vrachtwagen("J-709-WW", new Container(3, "Tapijten")),
+        new Vrachtwagen("Y-509-XX", new Container(4, "Iemands tiny house")),
+        new Vrachtwagen("U-683-JC", new Container(5, "Banden")),
+        new Vrachtwagen("M-996-SK", new Container(6, "Oude kaas")),
+        new Vrachtwagen("C-114-ZF", new Container(7, "Schoenen")),
+        new Vrachtwagen("J-550-TL", new Container(8, "3D-printers")),
+        new Vrachtwagen("X-788-WP", new Container(9, "Sambalmansambal")),
+        new Vrachtwagen("G-708-WW", new Container(10, "Speciaalbier")),
+        new Vrachtwagen("A-744-RT", new Container(11, "'Bananen'")),
+        new Vrachtwagen("Q-491-NA", new Container(12, "De F1-auto van Max Verstappen")),
+        new Vrachtwagen("F-094-YN", new Container(13, "Middelradioactief afval (goed ingepakt)")),
+        new Vrachtwagen("P-102-AA", new Container(14, "Tulpen")),
+        new Vrachtwagen("Z-111-ZZ", new Container(15, "Tandartsstoel")),
+        new Vrachtwagen("V-676-EG", new Container(16, "Turbo Encabulators")),
+        new Vrachtwagen("J-263-PR", new Container(17, "Disco")),
+        new Vrachtwagen("G-900-IO", new Container(18, "Handbalballen")),
+        new Vrachtwagen("A-009-PP", new Container(19, "Koffiebonen")),
+        new Vrachtwagen("I-610-TO", new Container(20, "Plastic koffiebekers")),
+        new Vrachtwagen("J-708-WW", new Container(21, "Niet-gedeclareerde variabelen")),
+        new Vrachtwagen("Q-010-IO", new Container(22, "Horloges")),
+        new Vrachtwagen("M-109-KL", new Container(23, "Lege pallets")),
+        new Vrachtwagen("Z-111-ZZ", new Container(24, "'Poedersuiker'")),
+        new Vrachtwagen("E-456-AO", new Container(25, "Motorolie")),
+        new Vrachtwagen("H-253-TE", new Container(26, "De Lord of the Rings Trilogie (extended cut+deleted scenes)")),
+        new Vrachtwagen("C-089-OA", new Container(27, "Drones")),
+        new Vrachtwagen("P-435-RO", new Container(28, "Videokaarten")),
+        new Vrachtwagen("U-780-MU", new Container(29, "To-to-to-tomaten"))
+    };
+}
