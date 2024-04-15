1c3f07991322011366657b0fe7d13b2d22e7c086
Matej Skelo <matej@skelo.nl>
2024-04-15 15:51:22 +0200

    Lesoefening les 3

--- java4/w2/les3/2 - Oefening/Uitwerking/App.java
+++ java4/w2/les3/2 - Oefening/Uitwerking/App.java
@@ -39,23 +39,29 @@ public class App {
         final int COUNT_PER_THREAD = 10;
         Counter c = new Counter();
         /**
-         * Dit is te langzaam. Dit kan sneller zonder loop, met parallelle Threads.
-         * Nu start dit steeds 1 Thread achter elkaar totdat MAX_COUNT is bereikt.
+         * Langzaam. Dit kan sneller.
+         * Nu start dit steeds 1 Thread achter elkaar totdat de 500 is bereikt.
          */
-        while (Counter.counter < MAX_COUNT) {
-            /**
-             * Door steeds hetzelfde Counter Object (Counter c) door te geven, 
-             * begint hij _niet_ steeds opnieuw met tellen.
-             */
-            CountThread ct = new CountThread(c, COUNT_PER_THREAD); 
-            ct.start();
-            /**
-             * sleep() is nodig in deze versie, omdat hij anders een nieuwe Thread start, 
-             * terwijl een andere misschien de 500 nog moet bereiken
-             * Als je meerdere parallelle Threads gebruikt (ipv achter elkaar zoals nu) is sleep() niet nodig
-             */
-            try                            { Thread.sleep(10); } 
-            catch (InterruptedException e) { e.printStackTrace(); }
+        // while (Counter.counter < MAX_COUNT) {
+        //     /**
+        //      * Door steeds hetzelfde Counter Object (Counter c) door te geven, 
+        //      * begint hij _niet_ steeds opnieuw met tellen.
+        //      */
+        //     CountThread ct = new CountThread(c, COUNT_PER_THREAD); 
+        //     ct.start();
+        //     /**
+        //      * sleep() is nodig in deze versie, omdat hij anders een nieuwe Thread start, 
+        //      * terwijl een andere misschien de 500 nog moet bereiken
+        //      * Als je meerdere parallelle Threads gebruikt (ipv achter elkaar zoals nu) is sleep() niet nodig
+        //      */
+        //     try                            { Thread.sleep(10); } 
+        //     catch (InterruptedException e) { e.printStackTrace(); }
+        // }
+
+        CountThread threads[] = new CountThread[MAX_COUNT/COUNT_PER_THREAD];
+        for (int i=0; i < MAX_COUNT/COUNT_PER_THREAD; i++) {
+            threads[i] = new CountThread(c, COUNT_PER_THREAD);
+            threads[i].start();
         }
     }
 }
