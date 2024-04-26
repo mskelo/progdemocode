15ef8e6350737f4ba6b05cbed83365c245a8ae92
Skelo <skelm@hr.nl>
2024-04-26 14:47:44 +0200

    SPO af

--- java4/w3/SPO/App.java
+++ java4/w3/SPO/App.java
@@ -16,12 +16,9 @@ public class App {
     public static void main(String[] args) {
         List<String> lines = new ArrayList<>();
         List<Vrachtwagen> kade = new ArrayList<>();
-        
-        // Lees het inputbestand uit. Dit heb je vorige periode al eens geschreven.
-        // TODO
-        
-        // Elke regel is een Vrachtwagen. Vul de kade met Vrachtwagens.
-        // TODO
+
+        // I/O
+
 
         /**
          * Knip de grote taak op in kleinere taken (van ~gelijke grootte) voor parallelle executie
@@ -32,19 +29,6 @@ public class App {
         final int AANTAL_VRACHTWAGENS = kade.size();        
         final int RIJSTROKEN = AANTAL_VRACHTWAGENS / MAX_VRACHTWAGENS_PER_RIJ;
 
-        // Kleine breinbreker: 
-        // We willen ieter 'stukje' van de List in een aparte Thread afhandelen
-        // Knip hier de `List kade` op in kleinere, even grote Lists.
-        // Gebruik hier bovenstaande variabelen voor.
-        
-        // TODO
-
-        // Maak en vul Douane Threads met voorgesorteerde Vrachtwagens
-        Thread[] douanecontrole = new Douane[RIJSTROKEN];
-        // TODO
-
-        // Start de Threads
-        // TODO
     }
 }
  
